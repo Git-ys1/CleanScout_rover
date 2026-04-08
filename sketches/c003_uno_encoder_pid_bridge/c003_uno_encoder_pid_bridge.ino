d95efa097516b8f92efd7ff336a3cb952e7a08c9
Git-ys1 <3091964993@qq.com>
2026-04-08 18:42:16 +0800

    C-2.2.8A：新增 M4 原始探测并冻结 M3/M4 诊断文档

--- sketches/c003_uno_encoder_pid_bridge/c003_uno_encoder_pid_bridge.ino
+++ sketches/c003_uno_encoder_pid_bridge/c003_uno_encoder_pid_bridge.ino
@@ -1,8 +1,14 @@
 #include <Tyler_1.h>
 #include <CsrBaseController.h>
+#include <WheelEncoder.h>
+
+#ifndef CSR_WHEEL4_RAW_PROBE
+#define CSR_WHEEL4_RAW_PROBE 0
+#endif
 
 static const unsigned long SERIAL_BAUD = 115200UL;
 static const unsigned long LED_PULSE_MS = 60UL;
+static const unsigned long RAW4_REPORT_MS = 100UL;
 
 static const float D_MECH_MM = 80.0f;
 static const float D_EFF_MM = 78.0f;
@@ -24,13 +30,15 @@ static const float WHEEL_OUTPUT_LIMIT = 255.0f;
 
 static const int8_t ENC_SIGN_W1 = 1;
 static const int8_t ENC_SIGN_W2 = 1;
-static const int8_t ENC_SIGN_W3 = 1;
+static const int8_t ENC_SIGN_W3 = -1;
 static const int8_t ENC_SIGN_W4 = 1;
 
 Tyler_1 tyler_1(1, 0, 1, 1, 200, 255, 255, 255);
 CsrBaseController controller(tyler_1, Serial);
+WheelEncoder wheel4Probe;
 
 unsigned long ledOffAtMs = 0;
+unsigned long raw4LastReportAtMs = 0;
 
 static CsrBaseControllerConfig makeControllerConfig() {
   CsrBaseControllerConfig config = {};
@@ -92,17 +100,34 @@ void setup() {
   Serial.begin(SERIAL_BAUD);
   delay(100);
 
+#if CSR_WHEEL4_RAW_PROBE
+  wheel4Probe.begin(3, 9, 10, ENC_SIGN_W4);
+#else
   controller.begin(controllerConfig);
+#endif
   SerialProtocol::sendReady(Serial);
 }
 
 void loop() {
   unsigned long nowMs = millis();
+
+#if CSR_WHEEL4_RAW_PROBE
+  if ((nowMs - raw4LastReportAtMs) >= RAW4_REPORT_MS) {
+    raw4LastReportAtMs = nowMs;
+    Serial.print(F("RAW4,"));
+    Serial.print(wheel4Probe.readTicks());
+    Serial.print(',');
+    Serial.print(digitalRead(9));
+    Serial.print(',');
+    Serial.println(digitalRead(10));
+  }
+#else
   controller.update(nowMs);
 
   if (controller.consumeAckPulse()) {
     pulseStatusLed();
   }
+#endif
 
   updateStatusLed();
 }
