90f805998ccfabe21a5f51bc5ad23ef1a47d945b
Git-ys1 <3091964993@qq.com>
2026-04-19 21:54:54 +0800

    V-1.7.12: 发布微信小程序 AppID 构建热修

--- vue3/scripts/release-mp-weixin.ps1
+++ vue3/scripts/release-mp-weixin.ps1
@@ -32,8 +32,22 @@ if ($LASTEXITCODE -ne 0) {
 
 $artifactDir = Join-Path $repoRoot 'dist\build\mp-weixin'
 $resolvedArtifactDir = [System.IO.Path]::GetFullPath($artifactDir)
+$projectConfigPath = Join-Path $artifactDir 'project.config.json'
+
+if (-not (Test-Path $projectConfigPath)) {
+  Write-Error 'project.config.json was not generated.'
+  exit 1
+}
+
+$projectConfig = Get-Content -Encoding utf8 $projectConfigPath -Raw | ConvertFrom-Json
+
+if ($projectConfig.appid -ne 'wxce1a2e91132f4c41') {
+  Write-Error "Unexpected mp-weixin appid: $($projectConfig.appid). Expected wxce1a2e91132f4c41."
+  exit 1
+}
 
 Write-Host ''
 Write-Host 'Mini program build finished.'
 Write-Host "Artifact directory: $resolvedArtifactDir"
+Write-Host "AppID: $($projectConfig.appid)"
 Write-Host 'Next step: import that dist/build/mp-weixin directory into WeChat DevTools.'
